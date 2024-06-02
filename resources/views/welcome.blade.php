e2ee6e7e8d22f0bfa15966ff9bb8930e62817a14
qiffym <qiww.dev@gmail.com>
2024-06-02 23:09:27 +0700

    fix donation page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Laravel Midtrans</title>
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
@@ -40,7 +40,43 @@
         </div>
     </div>
 
+    <div class="container px-4 py-5">
+        <form class="row g-3">
+            <legend>Donation</legend>
+            <div class="col-md-6">
+                <label for="name" class="form-label">Nama</label>
+                <input type="text" name="name" class="form-control" id="name">
+            </div>
+            <div class="col-md-6">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" name="email" class="form-control" id="email">
+            </div>
+            <div class="col-md-12">
+                <label for="donation_type" class="form-label">Jenis Donasi</label>
+                <select name="donation_type" id="donation_type" class="form-select">
+                    <option selected>Pilih jenis donasi</option>
+                    <option value="medis_kesehatan">Medis & Kesehatan</option>
+                    <option value="kemanusiaan">Kemanusiaan</option>
+                    <option value="bencana_alam">Bencana Alam</option>
+                    <option value="rumah_ibadah">Rumah Ibadah</option>
+                    <option value="beasiswa_pendidikan">Beasiswa & Pendidikan</option>
+                    <option value="sarana_infrastruktur">Sarana & Infrastruktur</option>
+                </select>
+            </div>
+            <div class="col-md-6">
+                <label for="amount" class="form-label">City</label>
+                <input type="number" name="amount" class="form-control" id="amount">
+            </div>
+            <div class="col-md-6">
+                <label for="note" class="form-label">Catatan (optional)</label>
+                <textarea name="note" class="form-control" id="note" rows="3"></textarea>
+            </div>
 
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary">Kirim</button>
+            </div>
+        </form>
+    </div>
 
 
 
